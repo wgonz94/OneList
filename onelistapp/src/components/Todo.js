de8bc3dffb646a5f7c85c2de6f93e5002e20582a
William Gonzales <wgonz94@yahoo.com>
2020-09-24 13:49:45 -0700

    set up handlers for todo

--- onelistapp/src/components/Todo.js
+++ onelistapp/src/components/Todo.js
@@ -1,16 +1,36 @@
 import React from "react";
 
 
-const Todo = () => {
+const Todo = ({ text, todo, todos, setTodos }) => {
     
     //complete handler
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id) {
+                return{
+                    ...item,
+                    completed: !item.completed
+                };
+            }
+            return item;
+        })
+        );
+    };
 
     //delete handler
-
+    const deleteHandler = () => {
+        setTodos(todos.filter((item) => item.id !== todo.id));
+    };
 
     return (
         <div className="todo">
-
+             <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
 
         </div>
     );
