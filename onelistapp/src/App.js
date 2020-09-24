91b313697e0531fbeee6d1ea366e9d866f15a43c
William Gonzales <wgonz94@yahoo.com>
2020-09-24 12:35:52 -0700

    setting up outline for form on app.js

--- onelistapp/src/App.js
+++ onelistapp/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//imported components
+
+
 function App() {
-  return (
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <header>
+      <h1>One List</h1>
+      <h4>Things To Do</h4>
       </header>
     </div>
-  );
-}
 
-export default App;
+    //place form here with states for todos, setTodos, inputs, setInputs, setStatus
+
+    //place list of todos here to filter based on status of todos
+  )
+}
