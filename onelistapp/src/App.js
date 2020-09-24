fce6ebe3acbb4efa8c6ba2385617fa67ea8f7473
William Gonzales <wgonz94@yahoo.com>
2020-09-24 13:21:16 -0700

    set up initial state management for todos and their inputs, and to input the status for filtered todos, along with displaying the filtered todos... also added export for app...

--- onelistapp/src/App.js
+++ onelistapp/src/App.js
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //imported components
-
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+
+  //State Management
+  const [todos, setTodos] = useState([]);
+  const [input, setInputs] = useState("");
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  // Run useEffect to update todos and local storage
+
+  // filter handler
 
   return(
     <div className="App">
@@ -13,10 +24,25 @@ function App() {
       <h1>One List</h1>
       <h4>Things To Do</h4>
       </header>
+
+      {/* place form here with states for todos, setTodos, inputs, setInputs, setStatus */}
+      <Form 
+        todos={todos}
+        setTodos={setTodos}
+        input={input}
+        setInputs={setInputs}
+        setStatus={setStatus}
+        />
+
+    {/* place list of todos here to filter based on status of todos */}
+    <TodoList 
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+    />
     </div>
 
-    //place form here with states for todos, setTodos, inputs, setInputs, setStatus
-
-    //place list of todos here to filter based on status of todos
-  )
+  );
 }
+
+export default App;
